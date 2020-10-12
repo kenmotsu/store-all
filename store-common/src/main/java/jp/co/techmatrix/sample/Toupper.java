283c5ed761f6254408af5d50a5be8d04cc7bd7c6
監物 敬浩 （ソフトウェアエンジニアリング技術２課） <kenmotsu@tmx.local>
2020-10-12 15:16:14 +0900

    test

--- store-common/src/main/java/jp/co/techmatrix/sample/Toupper.java
+++ store-common/src/main/java/jp/co/techmatrix/sample/Toupper.java
@@ -5,9 +5,9 @@ public class Toupper {
 	public String value = null;
 	
 	public Toupper(int index) {
-//		if (index == 0) {
-//			value = "value is zero";
-//		}
+		if (index == 0) {
+			value = "value is zero";
+		}
 	}
 	
 	public String toUpper() {
--- store-front/src/main/java/jp/co/techmatrix/sample/ToupperCall.java
+++ store-front/src/main/java/jp/co/techmatrix/sample/ToupperCall.java
@@ -2,7 +2,7 @@ package jp.co.techmatrix.sample;
 
 public class ToupperCall {
 
-	public static void main(String[] args) {
+	public static void testNP() {
 		Toupper name = new Toupper(10);
 		System.out.println(name.toUpper());
 	}
