6fbd1024c12112211a7b4003396a2f4c7a7eec49
監物 敬浩 （ソフトウェアエンジニアリング技術２課） <kenmotsu@tmx.local>
2020-10-12 13:31:58 +0900

    test

---
+++ store-common/src/main/java/jp/co/techmatrix/sample/Toupper.java
@@ -0,0 +1,16 @@
+package jp.co.techmatrix.sample;
+
+public class Toupper {
+
+	public String value = null;
+	
+	public Toupper(int index) {
+		if (index == 0) {
+			value = "value is zero";
+		}
+	}
+	
+	public String toUpper() {
+		return value.toUpperCase();
+	}
+}
---
+++ store-front/src/main/java/jp/co/techmatrix/sample/ToupperCall.java
@@ -0,0 +1,9 @@
+package jp.co.techmatrix.sample;
+
+public class ToupperCall {
+
+	public static void testNP() {
+		Toupper name = new Toupper(10);
+		System.out.println(name.toUpper());
+	}
+}
